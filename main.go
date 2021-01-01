1adcdabec6e943049412c05a86dc90598bb30cf1
vanajaylakshmi <76813206+vanajaylakshmi@users.noreply.github.com>
2021-01-01 10:23:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -27,9 +27,22 @@ func main() {
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
-		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err := fmt.Scanln(&originValue)
+		if err != nil {
+			printError(errReadingInput)
+		}
 
-		if shouldConvertAgain != "Y" {
+		if originUnit[0] == 'C' {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		_, err = fmt.Scanf(shouldConvertAgain)
+		if err != nil {
+			printError(errReadingInput)
+		}
+		if strings.ToUpper(strings.TrimSpace(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
